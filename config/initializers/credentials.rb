7e91e416ec35235ea00fbbef162b70a308557097
Joao Santana <jsantana@thoughtworks.com>
2016-10-04 01:26:27 -0300

    Add missin require yaml

--- config/initializers/credentials.rb
+++ config/initializers/credentials.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 INSTAGRAM = YAML.load_file(Rails.root.join('config/instagram.yml'))[Rails.env]
 
 REDIRECT_URL = "#{INSTAGRAM['redirect_endpoint']}:#{INSTAGRAM['redirect_port']}/#{INSTAGRAM['redirect_path']}"
