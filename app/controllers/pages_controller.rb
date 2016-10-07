85366b85ed1a4148a5d863d10c4f580ffcb651f0
Diogo Lundberg <diogo@lundberg.com.br>
2016-10-07 01:49:09 -0300

    Refactor controller to insure that params always contains railsgirls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,32 +1,22 @@
 class PagesController < ApplicationController
+  before_action :set_pictures
   respond_to :json, only: :search
   respond_to :html, only: :index
 
   def index
-    session[:init] = true
     session[:access_token] = ENV['INSTAGRAM_ACCESS_TOKEN']
-    if session[:access_token].present?
-      @pictures = pictures_by_hashtag(params[:hashtag])
-    else
-      redirect_to login_path
-    end
   end
 
   def search
-    if params[:hashtag].include? 'railsgirls'
-      @pictures = InstagramClient.new(session[:access_token]).search(params[:hashtag])
-    else
-      @pictures = nil
-    end
     respond_with json: @pictures
   end
 
   private
-    def pictures_by_hashtag(hashtag)
-      if hashtag.nil? || !hashtag.include?('railsgirls')
-        InstagramClient.new(session[:access_token]).search('railsgirls')
+    def set_pictures
+      if params[:hashtag].include? 'railsgirls'
+        @pictures = InstagramClient.new(session[:access_token]).search(hashtag)
       else
-        InstagramClient.new(session[:access_token]).search(hashtag)
+        @pictures = nil
       end
     end
 end
